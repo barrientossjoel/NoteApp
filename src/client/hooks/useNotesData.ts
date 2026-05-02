98c8d6c378e2c138d90cb0da706feafe00561b13
barrientossjoel <barrientoss.joel@gmail.com>
2026-05-01 21:23:52 -0300

    fix: delete timer

--- src/client/hooks/useNotesData.ts
+++ src/client/hooks/useNotesData.ts
@@ -31,12 +31,8 @@ export function useNotesData() {
     const handleFocus = () => fetchData();
     window.addEventListener('focus', handleFocus);
     
-    // Also poll every 30 seconds
-    const interval = setInterval(fetchData, 30000);
-
     return () => {
       window.removeEventListener('focus', handleFocus);
-      clearInterval(interval);
     };
   }, [fetchData]);
 
